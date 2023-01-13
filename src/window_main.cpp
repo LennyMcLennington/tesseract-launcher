da273dec037e12e90c52e9fc72c2c59d541e5238
Lenny McLennington <lenny@sneed.church>
2023-01-13 01:17:17 +0000

    tmp

--- src/window_main.cpp
+++ src/window_main.cpp
@@ -1,5 +1,5 @@
 #include "window_main.h"
-#include "./ui_window_main.h"
+#include "ui_window_main.h"
 
 WindowMain::WindowMain(QWidget *parent): QMainWindow(parent), ui(new Ui::WindowMain) {
     ui->setupUi(this);
